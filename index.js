0d23e1786ca0e8c52c4e041e8db2f585281bccdf
SlavikLutsyk <101299986+SlavikLutsyk@users.noreply.github.com>
2024-12-29 21:21:49 +0200

    Ready task

---
+++ index.js
@@ -0,0 +1,92 @@
+function validSolution(board) {
+    if (!Array.isArray(board) || board.length !== 9 || !board.every(row => Array.isArray(row) && row.length === 9)) {
+      console.error("Error!");
+      return false;
+    }
+    for (let i = 0; i < 9; i++) {
+      if (board[i].includes(0)) return false;
+    }
+    function isValidLine(line) {
+      return line.slice().sort().join('') === '123456789';
+    }
+    for (let i = 0; i < 9; i++) {
+      const row = board[i];
+      const column = board.map(row => row[i]);
+      if (!isValidLine(row) || !isValidLine(column)) return false;
+    }
+    for (let row = 0; row < 9; row += 3) {
+      for (let col = 0; col < 9; col += 3) {
+        const block = [];
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 3; j++) {
+            block.push(board[row + i][col + j]);
+          }
+        }
+        if (!isValidLine(block)) return false;
+      }
+    }
+    return true;
+  }
+
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]));
+      
+   console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 8],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]
+  ]));
+
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 9, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]));
+
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 5, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ]));
+
+  console.log(validSolution(3));
+
+  console.log(validSolution([
+    [5, 3, 4, 6, 7, 5, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1]
+  ]));
+  
